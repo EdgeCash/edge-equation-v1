f64ee080ca188cd492e61589a148599b4ec3c625
EdgeCash <professoredgecash@gmail.com>
2026-04-19 13:14:24 -0500

    Create registry.py

---
+++ src/edge_equation/models/registry.py
@@ -0,0 +1,19 @@
+from typing import Dict, Type
+
+
+class BaseModelRunner:
+    name: str = "base"
+
+    def run(self, data) -> dict:
+        raise NotImplementedError
+
+
+class ModelRegistry:
+    def __init__(self) -> None:
+        self._models: Dict[str, Type[BaseModelRunner]] = {}
+
+    def register(self, model: Type[BaseModelRunner]) -> None:
+        self._models[model.name] = model
+
+    def get(self, name: str) -> Type[BaseModelRunner]:
+        return self._models[name]
