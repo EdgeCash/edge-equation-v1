b3d17ae8eae8f6159fe076fbf6d5081c39bd16dc
EdgeCash <professoredgecash@gmail.com>
2026-05-05 11:40:36 -0500

    Create assist_model.py

---
+++ src/edge_equation/engines/wnba/features/assist_model.py
@@ -0,0 +1,5 @@
+def assist_opportunity(assist_rate: float, possessions: float) -> float:
+    """
+    Estimate assist opportunities based on team assist rate and possessions.
+    """
+    return assist_rate * possessions
